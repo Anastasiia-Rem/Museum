fe08e70157c09fc1056da40953d5f42b05cab694
Anastasiia <anastasiia.remyzovska@gmail.com>
2026-09-07 13:34:55 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
 'use strict';
+
+const slider = document.querySelector('.gallery__slider');
+const slides = document.querySelectorAll('.gallery__slide');
+const dots = document.querySelectorAll('.gallery__dot');
+
+function setActiveDot(index) {
+  dots.forEach((dot) => {
+    dot.classList.remove('gallery__dot--active');
+  });
+
+  dots[index].classList.add('gallery__dot--active');
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    const slide = slides[index];
+
+    slider.scrollTo({
+      left: slide.offsetLeft,
+      behavior: 'smooth',
+    });
+
+    setActiveDot(index);
+  });
+});
+
+slider.addEventListener('scroll', () => {
+  let closestSlideIndex = 0;
+  let smallestDistance = Infinity;
+
+  slides.forEach((slide, index) => {
+    const distance = Math.abs(slide.offsetLeft - slider.scrollLeft);
+
+    if (distance < smallestDistance) {
+      smallestDistance = distance;
+      closestSlideIndex = index;
+    }
+  });
+
+  setActiveDot(closestSlideIndex);
+});
+
+const form = document.querySelector('.subscription__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
